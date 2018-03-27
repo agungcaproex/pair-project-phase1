dd04959bd871f21b72a808ce348b79f02c8f7a4f
yohanessahrul <yohanessahrul92@gmail.com>
2018-03-27 17:23:58 +0700

    CRUD minimarket

---
+++ seeders/20180327095433-add-data-minimarket.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+  //  return queryInterface.bulkInsert('Minimarkets', [
+  //    {
+  //      name: 'Indomaret Kebon Kacang',
+  //      address: 'JL Kebon Kacang No 57 A, Bekasi',
+  //      createdAt: new Date(),
+  //      updatedAt: new Date()
+  //    },
+  //    {
+  //      name: 'Alfamart Pondok Gede',
+  //      address: 'JL Jatiwaringin No 25, Bekasi',
+  //      createdAt: new Date(),
+  //      updatedAt: new Date()
+  //    },
+  //    {
+  //      name: 'Alfamidi Kampung Ambon',
+  //      address: 'JL Kampung AMbon, Rawa Mangun',
+  //      createdAt: new Date(),
+  //      updatedAt: new Date()
+  //    },
+  //    {
+  //      name: 'Indomaret Kelapa Gading',
+  //      address: 'JL KElapa Gading Nias',
+  //      createdAt: new Date(),
+  //      updatedAt: new Date()
+  //    },
+  //    {
+  //      name: 'Alfamidi Kebon Baru',
+  //      address: 'JL Sawah Besar Blok M',
+  //      createdAt: new Date(),
+  //      updatedAt: new Date()
+  //    }
+  //  ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    
+  }
+};
