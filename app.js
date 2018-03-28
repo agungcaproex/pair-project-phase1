600cc4b9c3acf7a17fe1e74583640a7460179e85
agungcaproex <agung.caproex@gmail.com>
2018-03-28 16:53:41 +0700

    upload heroku

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.set('port', (process.env.port || 3000))
+
 app.locals.helper = require('./helpers/helper')
 
 app.set('view engine', 'ejs');
