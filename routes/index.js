582b7548f3fcd9ec31ece070a34d9b212a56efcb
agungcaproex <agung.caproex@gmail.com>
2018-03-28 14:13:54 +0700

    add register user form

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const {User} = require('../models')
 
 router.get('/', function(req, res) {
-    res.send('Ini halaman Dashboard Pair Project')
+    res.render('register-login/home')
 })
 
 router.get('/home', function(req, res) {
@@ -9,11 +10,33 @@ router.get('/home', function(req, res) {
 })
 
 router.get('/login', function(req, res) {
-    res.send('Halaman Login')
+    res.render('register-login/home')
 })
 
+
 router.get('/register', function(req, res) {
-    res.send('Halaman daftar Netijen')
+    res.render('register-login/register')
+})
+
+router.post('/register', function(req, res) {
+    let newUser = {
+        username: req.body.username,
+        password: req.body.password,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        gender: req.body.gender,
+        email: req.body.email,
+        role: 'netizen'
+    }
+
+    User.create(newUser)
+    .then(dataUser => {
+        console.log('======', dataUser)
+        res.render('register-login/register_success')
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
 
 
